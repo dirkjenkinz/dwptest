e7bb5ef0e7fa2f1a1a60463382104730ff20665a
Dirk Jenkinz <dirkjenkinz@gmail.com>
2021-05-27 13:14:32 +0100

    handle_fetch_error

--- services/handlers/get-people-handler.js
+++ services/handlers/get-people-handler.js
@@ -3,13 +3,18 @@ const config = require('../../config/config');
 
 const getPeopleHandler = async (req, res) => {
     const url = config.getPeopledataUrl;
-    const response = await callAPI(url);
-    console.log(`API call status = ${response[0]}`);
-    if (response[0] === 200) {
-        res.locals.list = response[1];
-        res.render('people.html');
-    } else {
-        res.locals.error = response[0];
+    try {
+        const response = await callAPI(url);
+        console.log(`API call status = ${response[0]}`);
+        if (response[0] === 200) {
+            res.locals.list = response[1];
+            res.render('people.html');
+        } else {
+            res.locals.error = response[0];
+            res.render('error.html');
+        }
+    } catch (error) {
+        res.locals.error = 500;
         res.render('error.html');
     }
 };
